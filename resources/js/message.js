592b9e63a6a8af369ebed777b45790bca2ca8904
Anmol Shrestha <nmol.crestha@gmail.com>
2025-08-07 10:09:44 +0545

    Added message.js in vite config and added style to profile image preview

---
+++ resources/js/message.js
@@ -0,0 +1,16 @@
+function imagePreview(input,selector) {
+   if(input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+         $(selector).attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+   }
+ }
+
+//  ON DOM LOAD
+$(document).ready(function() {
+    $('#select_file').change(function() {
+        imagePreview(this, '.profile-image-preview');
+    });
+});
--- resources/views/messages/layouts/profile-modal.blade.php
+++ resources/views/messages/layouts/profile-modal.blade.php
@@ -4,8 +4,8 @@
             <div class="modal-body">
                 <form action="#">
 
-                    <div class="file">
-                        <img src="{{ asset(auth()->user()->avatar) }}" alt="Upload" class="img-fluid">
+                    <div class="file profile-file">
+                        <img src="{{ asset(auth()->user()->avatar) }}" alt="Upload" class="img-fluid profile-image-preview">
                         <label for="select_file"><i class="fal fa-camera-alt"></i></label>
                         <input id="select_file" type="file" hidden>
                     </div>
