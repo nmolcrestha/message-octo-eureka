4c710536f2cd9410e607df25f664594be5ccad4b
Anmol Shrestha <nmol.crestha@gmail.com>
2025-08-07 07:21:13 +0545

    added column in user table with ui changes in the message blade file

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string(column: 'user_name')->nullable();
+            $table->string('avatar')->default('/default/avatar.png');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
